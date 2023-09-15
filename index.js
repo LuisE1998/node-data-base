fc560877c6dc993fd82ac0528b1f2e066fcfa097
LuisE1998 <lerb11@hotmail.com>
2023-09-15 12:22:16 -0500

    Move the test

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Estoy ejecutando");
-
-function sum(a, b) {
-    return a + b;
-  }
-
-  module.exports = {
-    sum
-  };
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,4 @@
-const { sum } = require('./index');
-
+const { sum } = require('./sum');
 
 test('sumar 1 + 2 debe ser igual a 3', () => {
     expect(sum(1, 2)).toBe(3);
