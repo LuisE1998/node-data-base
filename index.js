afc6969b60a31e9f22e6778255ebb90d8b59b40f
LuisE1998 <lerb11@hotmail.com>
2023-09-18 18:11:39 -0500

    Add api to create school

--- index.js
+++ index.js
@@ -19,4 +19,14 @@ app.get('/', async (_req, res) => {
     return res.send(result.rows); 
 });    
 
+app.use(express.urlencoded({ extended: true }));
+app.post('/createSchool', async (req, res) => {
+    const schoolName = req.body.school_name;
+    const schoolEmail = req.body.school_email;
+    await client.connect();
+    const result = await client.query('Insert into school (school_name, school_email) values ($1, $2)', [schoolName, schoolEmail]);
+    await client.end();
+    return res.send(result.rows); 
+});   
+
 app.listen(8000, () => console.log("listeningg...."));
